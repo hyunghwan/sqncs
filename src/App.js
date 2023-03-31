938778e6edf6193bbdb17f15df03b203428c49ea
Hyunghwan Byun <hyunghwan@users.noreply.github.com>
2023-03-30 22:05:03 -0700

    initial version

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
+  Flex,
   Text,
+  Divider,
+  Button,
+  Image,
   Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 
-function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
-  );
-}
+const mailtosqncs = 'mailto:hello@sqncs.com';
+
+const App = () => (
+  <ChakraProvider resetCSS>
+    <Flex alignItems="center" flexDirection="column" mt={24}>
+      <Flex maxWidth={800} width="80%" flexDirection="column">
+        <Image boxSize="150px" src="sqncs.png" alt="sqncs logo" mb={4} />
+        <Text fontSize="4xl" fontWeight="bold" color="gray.900" mb={8}>
+          sqncs studio
+        </Text>
+        <Text fontSize="md" color="gray.600" mb={12}>
+          A software studio situated around the Pacific Ocean.
+        </Text>
+        <Divider borderColor="gray.200" mb={4} />
+        <Link href={mailtosqncs} isExternal>
+          <Button
+            variant="solid"
+            rightIcon={<ArrowForwardIcon />}
+            size="md"
+            colorScheme="gray"
+          >
+            Get in touch
+          </Button>
+        </Link>
+      </Flex>
+    </Flex>
+  </ChakraProvider>
+);
 
 export default App;
